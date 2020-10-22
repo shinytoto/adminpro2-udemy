ccb73487d5ab4dc045dafd1d8a00ecf1486c7551
Nicolas <nicolascordobacampos@gmail.com>
2020-10-21 20:48:34 -0500

    Fin sección 3.

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+})
+export class PagesComponent implements OnInit {
+  year = new Date().getFullYear();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
